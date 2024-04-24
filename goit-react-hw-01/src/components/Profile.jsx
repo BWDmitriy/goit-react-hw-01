13272198258366ee51680e038aba78da7dd33b90
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-04-24 17:30:50 +0300

    task 1 logic

---
+++ goit-react-hw-01/src/components/Profile.jsx
@@ -0,0 +1,32 @@
+const Profile = (prop) => {
+  return (
+    <div>
+        <div>
+            <img
+                src={prop.image}
+                alt="User avatar"
+            />
+            <p>{prop.name}</p>
+            <p>@{prop.tag}</p>
+            <p>{prop.location}</p>
+        </div>
+
+        <ul>
+            <li>
+                <span>Followers</span>
+                <span>{prop.stats.followers}</span>
+            </li>
+            <li>
+                <span>Views</span>
+                <span>{prop.stats.views}</span>
+            </li>
+            <li>
+                <span>Likes</span>
+                <span>{prop.stats.likes}</span>
+            </li>
+        </ul>
+    </div>
+);
+}
+
+export default Profile;
